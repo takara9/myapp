d63e3d1dc4b991ad150b90afe108babf35f0f8b7
Maho Takara <takara9@gmail.com>
2019-05-13 21:44:25 +0900

    add groovy

---
+++ nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS Job') {
+    scm {
+        git('https://https://github.com/takara9/node-express-login') {  node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@example.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs_v11')
+    }
+    steps {
+        shell("npm install")
+        shell("npm test")
+    }
+}
